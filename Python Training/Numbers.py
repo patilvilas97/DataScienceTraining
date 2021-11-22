4fbe235b601af71a751d22ee4a81f920d3af9ac6
Vilas Patil <patildipak422@gmail.com>
2021-11-22 20:22:22 +0530

    Numbers and Variables

---
+++ Python Training/Numbers.py
@@ -0,0 +1,13 @@
+num1 = 13
+num2 = 6
+division = num1/num2
+print("Division : ",division)
+print(round(division))
+print(round(division,2))
+print(round(division,3))
+print(round(division,4))
+
+remainder = num1%num2
+print("Remainder : ", remainder)
+
+print("Addition :", num1+num2)
---
+++ Python Training/Variables.py
@@ -0,0 +1,12 @@
+rent = 1220 ##assigning value
+gas = 202.5
+groceries = 350.6
+print(rent)
+total = rent + gas + groceries
+print(total)        ##printing Total
+
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+
+print("Expense Items : " ,item1,item2,item3) ##printing some stuff
