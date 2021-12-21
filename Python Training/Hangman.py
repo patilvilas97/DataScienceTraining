a2fd6a32de80272f286056391d3c626bcb625c63
Vilas Patil <patildipak422@gmail.com>
2021-12-22 00:00:08 +0530

    Completed the game with all the functions Comments have been added so one can understand the code

--- Python Training/Hangman.py
+++ Python Training/Hangman.py
@@ -1,25 +1,26 @@
-
-def checkLetter(question):
-    char = input("Enter a Character : ")[0]
+flag = False
+def checkLetter(question,flag):                                 ##To check whether the entered character is present in
+    char = input("Enter a Character : ")[0]                     ##the question, if present the user's guess is correct
     for i in range(len(question)):
         if question[i] == char:
             word[i] = char  ##abcdef
-            flag = True
+            flag =  True
     return flag
 
-def winnerOrLoser(word):
-    s = ""
+def winnerOrLoser(word):                                        ##To check whether our guestion is correctly solve or
+    s = ""                                                      ##is pending to complete
     for i in word:
         s += i
     if question==s:
         print("You are a Winner!!")
+        print(str(word))
+        print("------------------------------------------------------------------------------------")
+        print("------------------------------------End---------------------------------------------")
         return 1
 
 
-question = "elephant"
+question = "elephant"                                           ##Word to guess
 chances = len(question)
-# chances = guesses
-# temp="--------"
 word=["-","-","-","-","-","-","-","-"]
 while chances != 0 :
     flag = False
@@ -27,9 +28,12 @@ while chances != 0 :
     print("----------------------------------Start---------------------------------------------")
     print("Your chances left are :", chances)
     print(word)
-    flag = checkLetter(question)
-    if winnerOrLoser(word) == 1:
-        break
+    flag = checkLetter(question,flag)
     if flag==False:
         chances -= 1
-
+    if winnerOrLoser(word) == 1:
+        break
+if(chances==0):
+    print("                     Sorry, Better Luck Next Time")
+    print("------------------------------------------------------------------------------------")
+    print("-----------------------------------End----------------------------------------------")
