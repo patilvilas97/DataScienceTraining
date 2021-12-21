4f5d0d07ac76a69dd4d872d57b5d173f556988f9
Vilas Patil <patildipak422@gmail.com>
2021-12-21 20:12:44 +0530

    Creating New Game Hangman, Initial Commit

---
+++ Python Training/Hangman.py
@@ -0,0 +1,22 @@
+question = "elephant"
+chances = len(question)
+# chances = guesses
+# temp="--------"
+word=["-","-","-","-","-","-","-","-"]
+while chances != 0 :
+    print("------------------------------------------------------------------------------------")
+    print("----------------------------------Start---------------------------------------------")
+    print("Your chances left are :", chances)
+    print(word)
+    char = list(input("Enter a Character : ")[0])
+    for i in range(len(question)):
+        if question[i]==char:
+            print(word[i])
+            word[i] == char
+            print(word[i])
+
+    if question==word:
+        print("You are a Winner!!")
+        break
+    else:
+        chances = chances-1
