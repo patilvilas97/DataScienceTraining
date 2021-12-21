72444c20a51ef803a41819b71862265c8b364854
Vilas Patil <patildipak422@gmail.com>
2021-12-21 20:53:10 +0530

    Solved CHances error Now Chances are decreassing correctly

--- Python Training/Hangman.py
+++ Python Training/Hangman.py
@@ -1,22 +1,35 @@
+
+def checkLetter(question):
+    char = input("Enter a Character : ")[0]
+    for i in range(len(question)):
+        if question[i] == char:
+            word[i] = char  ##abcdef
+            flag = True
+    return flag
+
+def winnerOrLoser(word):
+    s = ""
+    for i in word:
+        s += i
+    if question==s:
+        print("You are a Winner!!")
+        return 1
+
+
 question = "elephant"
 chances = len(question)
 # chances = guesses
 # temp="--------"
 word=["-","-","-","-","-","-","-","-"]
 while chances != 0 :
+    flag = False
     print("------------------------------------------------------------------------------------")
     print("----------------------------------Start---------------------------------------------")
     print("Your chances left are :", chances)
     print(word)
-    char = list(input("Enter a Character : ")[0])
-    for i in range(len(question)):
-        if question[i]==char:
-            print(word[i])
-            word[i] == char         ##abcdef
-            print(word[i])
-
-    if question==word:
-        print("You are a Winner!!")
+    flag = checkLetter(question)
+    if winnerOrLoser(word) == 1:
         break
-    else:
-        chances = chances-1
+    if flag==False:
+        chances -= 1
+
