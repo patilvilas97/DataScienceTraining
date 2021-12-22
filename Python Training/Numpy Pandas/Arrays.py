f5a8593263b26f0d36ae136e056aa83c3bd7f8a0
Vilas Patil <patildipak422@gmail.com>
2021-12-22 18:45:02 +0530

    Created Array and Practice something on it

---
+++ Python Training/Numpy Pandas/Arrays.py
@@ -0,0 +1,15 @@
+import numpy as np                              #importing Numpy Package and renaming it as np
+
+numbers = np.array([1,2,3,4,5])                 ##Creating arrays anda adding values
+print(numbers)
+print(type(numbers))
+
+
+print(numbers[0])                               ##Printing Independently
+print(numbers[3])
+
+for i in numbers:                               ##Prinitng all the values from arrays
+    print(i, end=" ")
+print()
+for i in range(len(numbers)):
+    print("Index Value:", i,"Value:",numbers[i])
