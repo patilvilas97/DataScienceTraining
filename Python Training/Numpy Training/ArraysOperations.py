74c23e5a0f28543175861bc5cbcf14d6b3b53c77
Vilas Patil <patildipak422@gmail.com>
2021-12-23 21:03:33 +0530

    Trying adding two different arrays

---
+++ Python Training/Numpy Training/ArraysOperations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([1,2,1.3,4,2,], dtype=float)                     ##Initializing the array
+print(arr1)
+print(np.reciprocal(arr1))                           ##Creating reciprocals of the elements in array
+print(np.reciprocal(arr1))
+
+arr2 = np.array([23,56,78,22,98])                               ##initalizing the array
+print(arr1+arr2)                                                ## Adding the arrays
+
+print(arr1*arr2)                                                ##Multiplying the arrays
+
