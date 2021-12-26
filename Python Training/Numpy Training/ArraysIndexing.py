8ae66789cc55cc081179b264f2ab2b8601d98fe4
Vilas Patil <patildipak422@gmail.com>
2021-12-26 21:37:04 +0530

    Learning the Indexing operations of n dimensional arrays

---
+++ Python Training/Numpy Training/ArraysIndexing.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr.dtype)                                        ##Printing Data Type of array
+print(arr[0,1,2])                                       ##Arrays Indexing
+print(arr[1,1,1])
+
+print(arr[-1,-1,-1])                                    ##Printing Last element of the array
