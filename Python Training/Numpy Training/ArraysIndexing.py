1ab2aa8e7b68fc6b51339ab262b63faac1fec92f
Vilas Patil <patildipak422@gmail.com>
2021-12-29 22:22:53 +0530

    Some new array Operations

--- Python Training/Numpy Training/ArraysIndexing.py
+++ Python Training/Numpy Training/ArraysIndexing.py
@@ -1,5 +1,6 @@
 import numpy as np
 arr = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr)
 print(arr.dtype)                                        ##Printing Data Type of array
 print(arr[0,1,2])                                       ##Arrays Indexing
 print(arr[1,1,1])
--- Python Training/Numpy Training/ArraysOperations.py
+++ Python Training/Numpy Training/ArraysOperations.py
@@ -11,3 +11,42 @@ print(arr1+arr2)                                                ## Adding the ar
 print(arr1*arr2)                                                ##Multiplying the arrays
 
 print(arr1/arr2)                                                ##Dividing two arrays
+
+print(arr1.ndim)                                         ##To print the dimensions
+
+print(arr1.size)                                         ##To count the number of elements present in the array
+
+print(arr1.shape)                                        ##To print the row and column count of the array
+
+arr= np.zeros((3,3))                                       ## initializing all the elements with zeroes
+
+print(arr)
+
+arr= np.ones((3,3))                                       ## initializing all the elements with ones
+
+print(arr)
+
+arr3=np.arange(1,10)                                    ##Create an array with elements 1 to 9
+
+print(arr3)
+
+arr3=np.arange(1,10,2)                                  ##create an array with increament of 2 from 1 upto 9
+
+print(arr3)
+
+arr3=np.linspace(1,5,20)                                ##will create 20 numbers between 1 and 5
+
+print(arr3)
+
+print(arr)
+
+print(arr.ravel())                                      ##will convert any dimensional array to 1D
+
+print(arr3.min())                                       ##To find the min from the array
+
+print(arr3.max())                                       ##To find the max from the array
+
+print(arr3.sum())                                       ##To sum up the array
+arr[0,2]=7
+arr[1,1]=8
+print(arr.sum(axis=1))                                  ##Will sum up the rows and colums
