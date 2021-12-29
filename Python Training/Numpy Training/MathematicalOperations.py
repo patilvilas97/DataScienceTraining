b4dd1b5b6074d82e96799148603aeec7dab3e247
Vilas Patil <patildipak422@gmail.com>
2021-12-29 22:29:10 +0530

    Mathematic operations in Numpy

---
+++ Python Training/Numpy Training/MathematicalOperations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.ones((3,4))
+arr[0,2]=7
+arr[1,3]=9
+print(arr)
+
+print(np.sqrt(arr))                                     ##To find the square root of each element
+
+print(np.std(arr))                                      ##To find the Standard Deviation
+
+
+
