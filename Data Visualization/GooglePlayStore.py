3d912fb2d8a6dfdcf7a9d6f22ac39a5d8bc557d3
Vilas Patil <patildipak422@gmail.com>
2022-01-16 22:50:55 +0530

    First Visualization Model Google Play Store

---
+++ Data Visualization/GooglePlayStore.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("googleplaystore.csv")
+
+# print(data.shape)
+print(data.describe())
+print(data[data['Rating'] > 5])
+data.drop([10472],inplace=True)             ##Dropping the row having rating more than 5
+print(data[10470:10475])                    ## Verifying whther dropped or not
+print(data.info())
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.width', 2000)
+pd.set_option('display.max_rows', 10840)
+pd.set_option('display.width', 2000)
+
+
+threshold = len(data)*0.1                     ##To remove the column which is 90% empty
+data.dropna(thresh=threshold,axis=1, inplace=True)
+print(data.isnull().sum())
+
+def impute_median(series):
+    return series.fillna(series.median())
+data.Rating = data['Rating'].transform(impute_median)
+
+print(data.isnull().sum())
+data['Type'].fillna(str(data['Type'].mode().values[0]), inplace = True)
+data['Current Ver'].fillna(str(data['Current Ver'].mode().values[0]), inplace = True)
+data['Android Ver'].fillna(str(data['Android Ver'].mode().values[0]), inplace = True)
+print(data.isnull().sum())
+print(data['Current Ver'].mode())
+print(data['Android Ver'].mode())
+print(data.head(2000))
+print(data.describe())
+
+
+data['Price'] = data['Price'].apply(lambda x: str(x).replace('$', '')if '$' in str(x) else str(x))
+data['Price'] = data['Price'].apply(lambda x: float(x))
+data['Reviews'] = pd.to_numeric(data['Reviews'], errors='coerce')
+
+data['Installs'] = data['Installs'].apply(lambda x: str(x).replace('+', '') if '+' in str(x) else str(x))
+data['Installs'] = data['Installs'].apply(lambda x: str(x).replace(',', '')if ',' in str(x) else str(x))
+data['Installs'] = data['Installs'].apply(lambda x:float(x))
+print(data.head(10))
+print(data.describe())
+# data.boxplot()
+# plt.show()
