6d213df03d30c39afaeb3a3be7aa142dfea70e05
Vilas Patil <patildipak422@gmail.com>
2022-01-18 22:08:14 +0530

    Added some Execution

--- Data Visualization/GooglePlayStore.py
+++ Data Visualization/GooglePlayStore.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("googleplaystore.csv")
 
-# print(data.shape)
-print(data.describe())
-print(data[data['Rating'] > 5])
+print(data.shape)
+# print(data.describe())
+# print(data[data['Rating'] > 5])
 data.drop([10472],inplace=True)             ##Dropping the row having rating more than 5
-print(data[10470:10475])                    ## Verifying whther dropped or not
-print(data.info())
+# print(data[10470:10475])                    ## Verifying whther dropped or not
+# print(data.info())
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 2000)
 pd.set_option('display.max_rows', 10840)
@@ -18,21 +18,21 @@ pd.set_option('display.width', 2000)
 
 threshold = len(data)*0.1                                   ##Defining the Threshold ex 10%
 data.dropna(thresh=threshold,axis=1, inplace=True)          ##Dropping the column having empty cells more than 90%
-print(data.isnull().sum())                                  ##Checking the sum of the Empty Cells
+# print(data.isnull().sum())                                  ##Checking the sum of the Empty Cells
 
 def impute_median(series):
     return series.fillna(series.median())
 data.Rating = data['Rating'].transform(impute_median)
 
-print(data.isnull().sum())
+# print(data.isnull().sum())
 data['Type'].fillna(str(data['Type'].mode().values[0]), inplace = True)
 data['Current Ver'].fillna(str(data['Current Ver'].mode().values[0]), inplace = True)
 data['Android Ver'].fillna(str(data['Android Ver'].mode().values[0]), inplace = True)
-print(data.isnull().sum())
-print(data['Current Ver'].mode())
-print(data['Android Ver'].mode())
-print(data.head(2000))
-print(data.describe())
+# print(data.isnull().sum())
+# print(data['Current Ver'].mode())
+# print(data['Android Ver'].mode())
+# print(data.head(2000))
+# print(data.describe())
 
 
 data['Price'] = data['Price'].apply(lambda x: str(x).replace('$', '')if '$' in str(x) else str(x))
@@ -42,7 +42,7 @@ data['Reviews'] = pd.to_numeric(data['Reviews'], errors='coerce')
 data['Installs'] = data['Installs'].apply(lambda x: str(x).replace('+', '') if '+' in str(x) else str(x))
 data['Installs'] = data['Installs'].apply(lambda x: str(x).replace(',', '')if ',' in str(x) else str(x))
 data['Installs'] = data['Installs'].apply(lambda x:float(x))
-print(data.head(10))
-print(data.describe())
-# data.boxplot()
-# plt.show()
+print(data[data['Installs'] < 0.6].count())
+# print(data.describe())
+data.boxplot()
+plt.show()
