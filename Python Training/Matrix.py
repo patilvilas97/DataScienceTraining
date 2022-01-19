5af0cc3d2d11746aabade3b01867ee31cc98214a
Vilas Patil <patildipak422@gmail.com>
2022-01-19 16:07:03 +0530

    Updated data and dropped one row

---
+++ Python Training/Matrix.py
@@ -0,0 +1,11 @@
+def unRepeated(a,m,n):
+    for i in range(3):
+        for j in range(3):
+            print(a[i][j])
+
+
+
+import numpy as np
+a=np.array([[11,12,13],[14,15,11],[1,1,1]])
+print(a.shape)
+print(a)
