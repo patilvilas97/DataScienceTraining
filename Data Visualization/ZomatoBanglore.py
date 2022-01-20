1d7903e759e530cadcaeb322b45964719ce6327e
Vilas Patil <patildipak422@gmail.com>
2022-01-20 23:37:09 +0530

    Reading new CSV File ZomatoBanglore.csv

--- Data Visualization/ZomatoBanglore.py
+++ Data Visualization/ZomatoBanglore.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 
 data=pd.read_csv("ZomatoBanglore.csv")
-
+print(data.shape)
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', 2000)
+pd.set_option('display.max_rows', 10840)
+pd.set_option('display.width', 2000)
 print(data.head(10))
 print(data.tail(10))
