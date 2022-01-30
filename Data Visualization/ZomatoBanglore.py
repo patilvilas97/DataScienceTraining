d3482b8c088d4c1a5493c0ed98923a10f47eb2bd
Vilas Patil <patildipak422@gmail.com>
2022-01-30 09:51:43 +0530

    Filling the null data cellss

--- Data Visualization/ZomatoBanglore.py
+++ Data Visualization/ZomatoBanglore.py
@@ -15,9 +15,13 @@ def cleanRate(series):
     series = series.replace("/5", "", inplace=True)
     # print(series)
     return float(series)
-
+print(data['rate'].unique())
+data[data['rate']].fillna(data.mean())
+print(data.isnull().sum())
 data['rate'] = data['rate'].apply(lambda x: str(x).replace('/5', '')if '/5' in str(x) else str(x))
-data.rate = data['rate'].apply(lambda x: float(x))
+data['rate'] = data['rate'].apply(lambda x: str(x).replace('NEW', '')if 'NEW' in str(x) else str(x))
+data['rate'] = data['rate'].apply(lambda x: str(x).replace('-', '')if '-' in str(x) else str(x))
+data['rate'] = data['rate'].apply(lambda x: float(x))
 print(data.isnull().sum())
 print(data['rate'].dtype)
 # series = 4.1/5
