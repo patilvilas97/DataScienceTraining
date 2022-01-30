1b78efdb2af5df465f35a553697c071450d7acec
Vilas Patil <patildipak422@gmail.com>
2022-01-30 17:14:45 +0530

    Cleaned Approx Cost Column

--- Data Visualization/ZomatoBanglore.py
+++ Data Visualization/ZomatoBanglore.py
@@ -9,21 +9,34 @@ data.drop_duplicates(inplace=True)
 
 data.drop(columns=['url', 'address','book_table','phone','menu_item','reviews_list','listed_in(city)'], axis=1, inplace=True)
 print(data.shape)
-print(data.isnull().sum())
+# print(data.isnull().sum())
 
-def cleanRate(series):
-    series = series.replace("/5", "", inplace=True)
-    # print(series)
-    return float(series)
-print(data['rate'].unique())
-data[data['rate']].fillna(data.mean())
+def handleRate(value):
+    if(value == 'NEW' or value == '-'):
+        return np.nan
+    else:
+        value = str(value).split('/')
+        value = value[0]
+        return float(value)
+
+data['rate'] = data['rate'].apply(handleRate)
+data['rate'] = data['rate'].fillna(data['rate'].mean())
+
+print(data['approx_cost(for two people)'].unique())
+def handleApproxCost(value):
+    if ',' in str(value):
+        value = str(value).split(',')
+        value = value[0] + value[1]
+        return float(value)
+    elif value == 'nan' :
+        value = data['approx_cost(for two people)'].median()
+        return float(value)
+    return float(value)
+data['approx_cost(for two people)'] = data['approx_cost(for two people)'].apply(handleApproxCost)
+print(data['approx_cost(for two people)'].unique())
+# data['approx_cost(for two people)']= data['approx_cost(for two people)'].fillna(data['approx_cost(for two people)'].mean())
 print(data.isnull().sum())
-data['rate'] = data['rate'].apply(lambda x: str(x).replace('/5', '')if '/5' in str(x) else str(x))
-data['rate'] = data['rate'].apply(lambda x: str(x).replace('NEW', '')if 'NEW' in str(x) else str(x))
-data['rate'] = data['rate'].apply(lambda x: str(x).replace('-', '')if '-' in str(x) else str(x))
-data['rate'] = data['rate'].apply(lambda x: float(x))
+data.dropna(inplace=True)
 print(data.isnull().sum())
-print(data['rate'].dtype)
-# series = 4.1/5
-# series = series.replace("/5", "", inplace=True)
-# print(series)
+print(data['location'].unique())
+# print(data['rate'].dtype)
