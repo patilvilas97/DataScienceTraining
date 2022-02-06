603f1aa8505adc2e0bd4e941429d630a698e20cd
Vilas Patil <patildipak422@gmail.com>
2022-02-06 21:40:32 +0530

    commit

--- Machine Learning/LinearRegressionFirstTry.py
+++ Machine Learning/LinearRegressionFirstTry.py
@@ -1,2 +1,25 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn import linear_model as lm
+
+data = pd.read_csv('homeprices.csv')
+print(data)
+
+plt.scatter(data.area,data.price,color='red', marker='+')
+
+reg = lm.LinearRegression()
+reg.fit(data[['area']], data.price)
+plt.plot(data.area, reg.predict(data[['area']]), color=red)
+print(reg.predict([[5000]]))
+print(reg.coef_)
+print(reg.intercept_)
+
+data1 = pd.read_csv('areas.csv')
+print(data1)
+p = reg.predict(data1)
+data1['prices'] = p
+print(data1)
+
+data1.to_csv('HousePricePrediction.csv', index = False)
+plt.show()
