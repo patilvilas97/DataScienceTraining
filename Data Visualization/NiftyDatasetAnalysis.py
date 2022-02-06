2740cf7d23be4fc40291693efdcaac4f2744e12a
Vilas Patil <patildipak422@gmail.com>
2022-02-06 18:40:51 +0530

    Final Commit, Analyzed the Nifty50 Dataset

--- Data Visualization/NiftyDatasetAnalysis.py
+++ Data Visualization/NiftyDatasetAnalysis.py
@@ -10,7 +10,7 @@ pd.set_option('display.width', 2000)
 print(data['Open'].unique);
 
 print(data.isnull().sum())
-def HandleOpen(value):
+def HandleComma(value):
     if ',' in value:
         value = str(value).replace(',', '')
         # value = str(value).split(',')
@@ -19,6 +19,39 @@ def HandleOpen(value):
     else :
         return float(value)
 
-data.Open = data['Open'].apply(HandleOpen)
-print(data.head())
-print(data.h)
+data.Open = data['Open'].apply(HandleComma)
+data.High = data['High'].apply(HandleComma)
+data.Low = data['Low'].apply(HandleComma)
+data.LTP = data['LTP'].apply(HandleComma)
+# data['Chng'] = data['Chng'].apply(HandleComma)
+# data['Volume (lacs)'] = data['Volume (lacs)'].apply(HandleComma)
+data['Turnover (crs.)'] = data['Turnover (crs.)'].apply(HandleComma)
+data['52w H'] = data['52w H'].apply(HandleComma)
+data['52w L'] = data['52w L'].apply(HandleComma)
+# data['365 d % chng'] = data['365 d % chng'].apply(HandleComma)
+# data['30 d % chng'] = data['30 d % chng'].apply(HandleComma)
+print(data)
+print(data.describe())
+
+grp = data.groupby(['Symbol'])
+x = grp['52w H'].agg(np.mean)
+plt.plot(x, linewidth=2, color='red')
+plt.xticks(rotation=90)
+# plt.show()
+#
+
+data.plot.bar(y=['52w H', '52w L'], x='Symbol')
+plt.title('52 weeks Comparision')
+plt.ylabel("Price")
+plt.xlabel('Stocks')
+# plt.show()
+
+
+data.plot.bar(x='Symbol', y='% Chng')
+plt.title('Percentage in Change')
+plt.xlabel('Stocks')
+plt.ylabel('Percentage in Change')
+plt.show()
+# plt.plot(x=data[
+# ])
+
